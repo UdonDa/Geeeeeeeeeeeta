7f1663d52cfb7d5f7d8a8526509231975a003513
Udon <udoooon0727@gmail.com>
2017-12-10 05:01:26 +0900

    [add]連携

---
+++ Assets/Geeeeeeeeta/src/SetWeatherText.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace UnityEngine.XR.iOS {
+	public class SetWeatherText : MonoBehaviour {
+
+		RequestWetherAPI requestWetherAPI = new RequestWetherAPI();
+		private Text targetText; 
+
+		void Start () {	
+			
+		}
+		
+		void Update () {
+			
+			var isFinishedForText = requestWetherAPI.getIsFinishedForText ();
+			if (isFinishedForText) {
+				var weatherText = requestWetherAPI.getTomorrowWeather();
+				var userPlace = requestWetherAPI.getUserPlace ();
+				setWetherTexts (weatherText, userPlace);
+				Debug.Log ("マンげ！！！！！！！！！！！！！" + weatherText + " : " + userPlace);
+				requestWetherAPI.setIsFinishedForText ();
+			}
+		}
+
+		private void setWetherTexts(string text, string userPlace){
+			
+			this.targetText = this.GetComponent<Text>();
+			this.targetText.text = "明日の\n" + userPlace +"の天気は\n" + text;
+		}
+	}
+}
---
+++ Assets/Geeeeeeeeta/src/UserPushed.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityEngine.XR.iOS {
+	public class UserPushed {
+
+		public string message;
+
+		public UserPushed() {
+			
+		}
+
+		public UserPushed(string message) {
+			this.message = message;
+		}
+
+
+	}
+}
